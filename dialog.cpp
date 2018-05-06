b6679a5bccfa14cb3966be2425a74e095ca5deda
Mike <BLMdnst8@gmail.com>
2018-05-06 00:37:33 -0400

    Begining of GUI

---
+++ dialog.cpp
@@ -0,0 +1,31 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+
+Dialog::Dialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+
+    // Set the scene
+    scene = new QGraphicsScene(this);
+    ui->graphicsView->setScene(scene);
+
+    //Define some colors for the graph. Gray will define unknown colors
+    QBrush redBrush(Qt::red);
+    QBrush blueBrush(Qt::blue);
+    QBrush greenBrush(Qt::green);
+    QBrush grayBrush(Qt::gray);
+    QPen blackPen(Qt::black);
+
+    // TEST
+    scene->addEllipse(50,50, 100, 100, blackPen, redBrush);
+
+    // Make an array to hold nodes
+    //QGraphicsItem* map1 = new QgraphicsItem[10];
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
---
+++ graphs.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2018-05-05T23:27:28
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = graphs
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        dialog.cpp
+
+HEADERS  += dialog.h
+
+FORMS    += dialog.ui
